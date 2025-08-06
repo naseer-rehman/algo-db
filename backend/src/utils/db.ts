679916a1b5933ec3f9d885dfa9387906fe9bdc1a
Naseer Rehman <rehmn.nas@gmail.com>
2025-08-06 18:09:22 -0400

    Make db actually connect
    - i forgor

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -6,6 +6,7 @@ const database = new Client({
   user: Bun.env.DB_USER,
   password: Bun.env.DB_PASSWORD,
   port: parseInt(Bun.env.DB_PORT ?? ""),
+  database: Bun.env.DB_NAME,
 });
 
 await database.connect();
